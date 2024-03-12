db173ef85ea3995acbc5a648774d647346cd628d
ArivazhaganPandiyan <arivutamizhan@icloud.com>
2024-03-12 11:58:36 +0530

    first commit

---
+++ src/helpers/API.js
@@ -0,0 +1,5 @@
+// Server side URL
+export const API = 'https://urlshortener-server.onrender.com/' ;
+
+// client side URL for new password
+export const AppLink = 'https://65eff77b6c955e7426de84f6--stirring-salamander-4c3040.netlify.app/' ;
